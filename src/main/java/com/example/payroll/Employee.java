e2503a0726bb389de05031456bace2ae0125e191
Jesse <jwstiemann@gmail.com>
2022-07-13 19:51:06 -0400

    Second Commit: Several untested features

--- src/main/java/com/example/payroll/Employee.java
+++ src/main/java/com/example/payroll/Employee.java
@@ -10,16 +10,19 @@ import javax.persistence.Id;
 class Employee {
 
     private @Id @GeneratedValue Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
+
     private String role;
 
     Employee() {
 
     }
 
-    Employee(String name, String role) {
+    Employee(String firstName, String lastName, String role) {
 
-        this.name = name;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.role = role;
     }
 
@@ -28,9 +31,14 @@ class Employee {
         return this.id;
     }
 
-    public String getName() {
+    public String getFirstName() {
 
-        return this.name;
+        return this.firstName;
+    }
+
+    public String getLastName() {
+
+        return this.lastName;
     }
 
     public String getRole() {
@@ -38,19 +46,36 @@ class Employee {
         return this.role;
     }
 
+    public String getName() {
+
+        return this.firstName + " " + this.lastName;
+    }
+
     public void setId(Long id) {
 
         this.id = id;
     }
 
-    public void setName(String name) {
+    public void setFirstName(String firstName) {
 
-        this.name = name;
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+
+        this.lastName = lastName;
     }
 
     public void setRole(String role) {
 
         this.role = role;
+    }
+
+    public void setName(String name) {
+
+        String[] parts = name.split(" ");
+        this.firstName = parts[0];
+        this.lastName = parts[1];
     }
 
     @Override
@@ -69,20 +94,21 @@ class Employee {
         Employee employee = (Employee) o;
 
         return Objects.equals(this.id, employee.id) &&
-                Objects.equals(this.name, employee.name) &&
+                Objects.equals(this.firstName, employee.firstName) &&
+                Objects.equals(this.lastName, employee.lastName) &&
                 Objects.equals(this.role, employee.role);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(this.id, this.name, this.role);
+        return Objects.hash(this.id, this.firstName, this.lastName, this.role);
     }
 
     @Override
     public String toString() {
 
-        return "Employee{" + "id=" + this.id + ", name='" + this.name + "', role='"
-                + this.role + "'}";
+        return "Employee{" + "id=" + this.id + ", firstName='" + this.firstName
+                + ", lastName='" + this.lastName + "', role='" + this.role + "'}";
     }
 }
---
+++ src/main/java/com/example/payroll/OrderNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.payroll;
+
+public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException(Long id) {
+
+        super("Order " + id + " not found");
+    }
+}
